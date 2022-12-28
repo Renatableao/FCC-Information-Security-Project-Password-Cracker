1341e71f059c9ba1fb6c3d716f5747ba15277430
Replit user <>
2022-12-27 21:43:52 -0300

    first commit

--- main.py
+++ main.py
@@ -10,5 +10,6 @@ cracked_password2 = password_cracker.crack_sha1_hash(
     "dcc466796201f7232b22a03781110a8871fd038c", use_salts=True)
 print(cracked_password2)
 
+
 # Run unit tests automatically
 main(module='test_module', exit=False)
--- password_cracker.py
+++ password_cracker.py
@@ -1,4 +1,31 @@
 import hashlib
 
-def crack_sha1_hash(hash):
+def crack_sha1_hash(hash, use_salts = False):
+
+  file1 = open('top-10000-passwords.txt', 'r')
+
+  password_hash = {}
+
+  for line in file1:
+    line = line.replace("\n", "")
+    if use_salts:
+      file2 = open('known-salts.txt', 'r')
+      for salt in file2:
+        salt = salt.replace("\n", "")
+        salt_prep = salt + line
+        line_hash_prep = hashlib.sha1(salt_prep.encode()).hexdigest()
+        password_hash[line_hash_prep] = line
+        salt_app = line + salt
+        line_hash_app = hashlib.sha1(salt_app.encode()).hexdigest()
+        password_hash[line_hash_app] = line
+    else:
+      line_hash = hashlib.sha1(line.encode()).hexdigest()
+      password_hash[line_hash] = line
+      
+
+  if hash in password_hash:
+    return password_hash[hash]
+  return "PASSWORD NOT IN DATABASE"
     
+    
+      
